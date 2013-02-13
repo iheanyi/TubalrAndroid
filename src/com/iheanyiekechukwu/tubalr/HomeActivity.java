9a4485a77c2ba8106ebfe1f173abc0fead5c80f6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-02-13 14:43:55 -0500

    Some layout testing . . .

--- src/com/iheanyiekechukwu/tubalr/HomeActivity.java
+++ src/com/iheanyiekechukwu/tubalr/HomeActivity.java
@@ -1,16 +1,24 @@
 
 package com.iheanyiekechukwu.tubalr;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.MediaController;
 
 public class HomeActivity extends Activity {
-
+    MediaController controller;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        controller = (MediaController) findViewById(R.id.mediaController);
+        
+        //controller = new MediaController(this);
+        controller.show(50000);
     }
 
     @Override
@@ -18,6 +26,14 @@ public class HomeActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        controller.hide();
+        //mediaPlayer.stop();
+        //mediaPlayer.release();
     }
 
 }

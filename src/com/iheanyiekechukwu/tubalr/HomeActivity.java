f9177cf17d6e1cce5b51cbddde53ca30a9866835
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-02-14 15:02:42 -0500

    New activities created (login, settings, etc.)

--- src/com/iheanyiekechukwu/tubalr/HomeActivity.java
+++ src/com/iheanyiekechukwu/tubalr/HomeActivity.java
@@ -2,12 +2,23 @@
 package com.iheanyiekechukwu.tubalr;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.MediaController;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements OnItemClickListener {
     MediaController controller;
+    
+    private ListView menuListView;
+    private ArrayAdapter<String> menuAdapter;
+    private Resources res;
+    private String[] menuNames;
     
     
     @Override
@@ -15,10 +26,22 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         
-        controller = (MediaController) findViewById(R.id.mediaController);
+        //controller = (MediaController) findViewById(R.id.mediaController);
+        
+        menuListView = (ListView) findViewById(R.id.homeFixedListView);
+        
+        res = getResources();
+        
+        menuNames = res.getStringArray(R.array.home_menu);
+        
+        menuAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuNames);
+        menuListView.setAdapter(menuAdapter);
+        menuListView.setOnItemClickListener(this);
+        
+        
         
         //controller = new MediaController(this);
-        controller.show(50000);
+        //controller.show(50000);
     }
 
     @Override
@@ -31,9 +54,15 @@ public class HomeActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-        controller.hide();
+        //controller.hide();
         //mediaPlayer.stop();
         //mediaPlayer.release();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View v, int id, long arg3) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
